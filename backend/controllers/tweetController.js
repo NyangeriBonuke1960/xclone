ddb44859973b47022dee83e63a76c4fd507c66cd
NyangeriBonuke1960 <stevennyangeri@gmail.com>
2025-09-08 11:55:23 +0300

    upload photos and videos functionality using multer

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -3,7 +3,9 @@ const TweetUsecase = require('../usecases/tweetUsecase');
 class TweetController {
     async postTweet(req, res){
         try{
-            const {content, author, imageUrl, videoUrl, quotedTweet} = req.body;
+            const {content, author, quotedTweet} = req.body;
+            const imageUrl = req.files.imageUrl ? req.files.imageUrl[0].path : undefined
+            const videoUrl = req.files.videoUrl ? req.files.videoUrl[0].path : undefined
             const result = await TweetUsecase.postTweet({content, author, imageUrl, videoUrl, quotedTweet})
             if(result.error) {
                 return res.status(400).json({error: result.error});
--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const TweetController = require('../controllers/tweetController');
+const upload = require('../utils/multer.js')
 
-router.post('/tweet', TweetController.postTweet);
+router.post('/tweet', upload.fields([
+    { name: 'imageUrl', maxCount: 1 },
+    { name: 'videoUrl', maxCount: 1 }
+]), TweetController.postTweet);
 
 module.exports = router;
